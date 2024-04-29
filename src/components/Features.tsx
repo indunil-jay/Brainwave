2ba9b1814b3b123a5ee90417c7e6a5e9441f6cbf
indunil-jay <indunil.jayz@gmail.com>
2024-04-30 02:21:54 +0530

    fix bug in feature section and remove slider and add grid

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -1,40 +1,89 @@
-import { FaAngleRight, FaRegCircle, FaRegCircleDot } from "react-icons/fa6";
+import { FaAngleRight } from "react-icons/fa6";
 import Section from "./Section";
 import { benefits } from "../constants/data";
-import { useState } from "react";
+import { gradient } from "../assets";
 
 const Features = () => {
-  const [cardIndex, setCardIndex] = useState<number>(2);
-
-  const moveCard = (dotIndex: number) => {
-    setCardIndex((index) => {
-      if (index === benefits.length - 1 && index < 0) return 0;
-      return dotIndex;
-    });
-  };
-
   return (
     <Section id="features" styles="-mt-2" withBottomDividers={false}>
-      <div className="mt-20 z-20 isolate">
+      <div className="2xl:w-[72%] xl:w-[84%] w-[94%] mx-auto">
         <h2 className="h2 text-center px-[3%] sm:px-[12%] md:px-[20%] lg:px-[24%] xl:px-[24.5%] 2xl:px-[28%]">
           Chat Smarter, Not Harder with Brainwave
         </h2>
 
-        {/* slider */}
+        <div className="my-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 relative">
+          <div className="absolute top-[65%] left-[10%] -translate-x-1/2 -translate-y-1/2 -z-20 opacity-50  mix-blend-color-dodge">
+            <img src={gradient} alt="gradient-background-image" />
+          </div>
+          <div className="absolute top-[15%] right-[10%] -translate-x-1/2 -translate-y-1/2 -z-20 opacity-50  mix-blend-color-dodge">
+            <img src={gradient} alt="gradient-background-image" />
+          </div>
+          {benefits.map((card) => {
+            return (
+              <div
+                className="group relative h-[24rem] flex justify-center  p-2 border rounded-3xl  shadow-2xl shadow-blue-500/20"
+                key={card.id}
+              >
+                <div className="p-[2.4rem] flex flex-col justify-around  rounded-3xl">
+                  <div>
+                    <h3 className="font-medium text-[1.5rem] leading-7 mb-5 capitalize">
+                      {card.title}
+                    </h3>
+                    <p className="mb-6 text-light-3 font-base leading-6 font-light">
+                      {card.text}
+                    </p>
+                  </div>
+
+                  <div className="flex justify-between items-center ">
+                    <div className="w-[3rem] h-[3rem]">
+                      <img
+                        src={card.iconUrl}
+                        className="w-full h-full object-contain"
+                      />
+                    </div>
+
+                    <div className="flex items-center cursor-pointer  px-3 py-3 rounded-xl gap-4 ">
+                      <a className="font-code uppercase text-xs font-semibold">
+                        {" "}
+                        Explore more
+                      </a>
+                      <FaAngleRight fontSize={16} />
+                    </div>
+                  </div>
+                </div>
+
+                <div className="absolute rounded-3xl -z-10 overflow-hidden  inset-0  opacity-0 transition-opacity group-hover:opacity-10 ">
+                  <img
+                    src={card.imageUrl}
+                    alt="background image"
+                    className="w-full h-full object-contain "
+                  />
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+
+      {/* <div className="mt-20 z-20 isolate">
+        <h2 className="h2 text-center px-[3%] sm:px-[12%] md:px-[20%] lg:px-[24%] xl:px-[24.5%] 2xl:px-[28%]">
+          Chat Smarter, Not Harder with Brainwave
+        </h2>
+
+     
         <div className="mb-20 mt-20">
-          <div className="relative  w-[600%]  xl:w-[300%] z-50  flex items-center ">
-            <div
-              className="flex gap-10 mb-10   md:translate-x-[16rem]   lg:translate-x-[24rem]  isolate   xl:translate-x-[34rem] "
-              style={{ translate: `${-18 * cardIndex}%` }}
-            >
-              {/* card */}
+      
+
+          <div className="w-full ">
+            <div className="grid grid-cols-3">
               {benefits.map((card) => {
                 return (
                   <div
-                    className="group relative block w-[24rem] p-2 border rounded-3xl  shadow-2xl shadow-blue-500/20"
+                    className="group relative flex items-center justify-center flex-shrink-0   flex-grow-0 w-full h-full p-2 border rounded-3xl  shadow-2xl shadow-blue-500/20"
                     key={card.id}
                   >
-                    <div className="min-h-[22rem]  bg- p-[2.4rem] flex flex-col justify-around  rounded-3xl">
+              
+                    <div className="p-[2.4rem] flex flex-col justify-around  rounded-3xl">
                       <div>
                         <h3 className="font-medium text-[1.5rem] leading-7 mb-5 capitalize">
                           {card.title}
@@ -62,6 +111,7 @@ const Features = () => {
                       </div>
                     </div>
 
+                  
                     <div className="absolute rounded-3xl -z-10 overflow-hidden  inset-0  opacity-0 transition-opacity group-hover:opacity-10 ">
                       <img
                         src={card.imageUrl}
@@ -74,16 +124,8 @@ const Features = () => {
               })}
             </div>
           </div>
-          {/* slider dots */}
-          <div className="flex gap-4 justify-center">
-            {benefits.map((_, index) => (
-              <button key={index} onClick={() => moveCard(index)}>
-                {cardIndex === index ? <FaRegCircleDot /> : <FaRegCircle />}
-              </button>
-            ))}
-          </div>
         </div>
-      </div>
+      </div> */}
     </Section>
   );
 };
