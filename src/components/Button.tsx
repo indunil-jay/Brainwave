7ed781ac21d089a7e4a6c884c50c3202786fb87d
indunil-jay <indunil.jayz@gmail.com>
2024-04-30 01:20:28 +0530

    fixaccibility issue in buttons

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,4 +1,5 @@
 import { LiaArrowRightSolid } from "react-icons/lia";
+import { Link } from "react-router-dom";
 
 type Props = {
   children: React.ReactNode;
@@ -7,6 +8,7 @@ type Props = {
   withArrow?: boolean;
   whiteBtn?: boolean;
   onClick?: () => void;
+  ariaLabel?: string; // New prop for providing custom aria-label
 };
 
 const Button = ({
@@ -16,34 +18,34 @@ const Button = ({
   onClick,
   withArrow = false,
   whiteBtn,
+  ariaLabel,
 }: Props) => {
   const white = whiteBtn
     ? "hover:text-dark-1 hover:bg-light-1"
     : "hover:text-light-1";
 
   const classBtn =
-    ` group outline-none rounded-md font-code font-semibold py-2 xl:py-3  px-3.5  xl:px-4  2xl:px-6  flex items-center  text-xs leading-5 uppercase transition-all  border  border-light-1/50 ${styles} ` +
+    `group outline-none focus:outline-none focus-visible:ring   rounded-md font-code font-semibold py-2 xl:py-3 px-3.5 xl:px-4 2xl:px-6 flex items-center text-xs leading-5 uppercase transition-all border border-light-1/50 ${styles} ` +
     white;
 
   const classSpan = `inline-block ${
-    withArrow ? "flex items-center  gap-2" : ""
-  } `;
+    withArrow ? "flex items-center gap-2" : ""
+  }`;
 
   if (href) {
     return (
-      <a href={href} className={classBtn}>
+      <Link to={href} className={classBtn} aria-label={ariaLabel}>
         <span className={classSpan}>
           {children}
           {withArrow && <LiaArrowRightSolid />}
         </span>
-      </a>
+      </Link>
     );
   }
 
   return (
-    <button className={classBtn} onClick={onClick}>
+    <button className={classBtn} onClick={onClick} aria-label={ariaLabel}>
       <span className={classSpan}>
-        {" "}
         {children}
         {withArrow && <LiaArrowRightSolid />}
       </span>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { disablePageScroll, enablePageScroll } from "scroll-lock";
 import { useState } from "react";
 import { brainwave } from "../assets";
@@ -30,21 +30,30 @@ const Header = () => {
   return (
     <>
       <header className="fixed top-0 left-0 right-0 px-6 py-4 z-50 xl:px-8 2xl:px-10 bg-dark-1/90  border-b border-b-stroke-1 backdrop-blur-sm ">
-        <div className="grid items-center   grid-cols-[1fr_1fr]  lg:grid-cols-[14rem_1fr_14rem]    xl:grid-cols-[20rem_1fr_20rem] 2xl:grid-cols-[25rem_1fr_25rem]">
+        <div className="grid items-center  grid-cols-[1fr_1fr]  lg:grid-cols-[14rem_1fr_14rem]  xl:grid-cols-[20rem_1fr_20rem] 2xl:grid-cols-[25rem_1fr_25rem]">
           {/* logo */}
           <div className="w-[9.5rem] justify-self-start lg:w-[10rem] xl:q-[12rem]  2xl:w-[14rem] h-auto flex items-center">
-            <a href="/" className="inline-block leading-6">
-              <img src={brainwave} className="h-full w-full object-contain" />
-            </a>
+            <Link
+              to="/"
+              className="inline-block leading-6"
+              aria-label="button-for-home-page"
+            >
+              <img
+                src={brainwave}
+                className="h-full w-full object-contain"
+                alt="brainwave-company-logo"
+              />
+            </Link>
           </div>
           {/* nav */}
           <nav className="hidden relative lg:block justify-self-center">
             <div className="flex items-center lg:gap-5 xl:gap-9">
               {navigation.map((item) => (
-                <a
+                <Link
                   key={item.id}
+                  aria-label={`button for go to ${item.name}`}
                   onClick={handleClick}
-                  href={item.url}
+                  to={item.url}
                   className={`${
                     item.onlyMobile ? "hidden" : "inline-block"
                   }  lg:px-5 xl:px-8 py-4 font-code uppercase text-xs font-semibold transition-colors hover:text-primary-1 leading-5 ${
@@ -52,28 +61,34 @@ const Header = () => {
                   }`}
                 >
                   {item.name}
-                </a>
+                </Link>
               ))}
             </div>
           </nav>
 
-          {/* theme toggler */}
-          <div className="flex justify-self-end  items-center justify-end gap-9">
+          <div
+            className="flex justify-self-end items-center justify-end gap-9 "
+            aria-label="nav-buttos-container"
+          >
             <Button
-              styles="text-light-1/50 hidden md:block  border-none"
-              href="/#signin"
+              styles="text-light-1/50 hidden md:visible md:block  border-none"
+              href="/#signup"
+              ariaLabel="signup-button"
             >
               New Account
             </Button>
             <Button
-              href="/#signup"
-              styles="hover:text-primary-1 hover:border-primary-1/50 hidden md:block"
+              href="/#signin"
+              styles="hover:text-primary-1 hover:border-primary-1/50 hidden md:block md:visible"
+              ariaLabel="signin-button"
             >
               Sign in
             </Button>
+
             <Button
               styles="lg:hidden py-2  hover:border-light-1 "
               onClick={handleToggleMenu}
+              ariaLabel="hamburger-menu"
             >
               <HamburgerMenu isOpen={isOpenNavigation} />
             </Button>
