ecb911c8b4431219fa612b8b23f5042705307db1
indunil-jay <indunil.jayz@gmail.com>
2024-05-03 03:12:33 +0530

    fix page refresh  not found error

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,6 @@
 import { Suspense, lazy } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SpinnerFullPage from "./components/SpinnerFullPage";
-// import LandingPage from "./pages/LandingPage";
-// import Signin from "./pages/Signin";
-// import Signup from "./pages/Signup";
-// import FeaturePage from "./pages/FeaturePage";
-// import RoadMap from "./pages/RoadMap";
-// import PricePage from "./pages/PricePage";
-// import HowToUsePage from "./pages/HowToUsePage";
 import PageNavigator from "./components/PageNavigator";
 
 const LandingPage = lazy(() => import("./pages/LandingPage"));
@@ -25,12 +18,12 @@ const App = () => {
         <PageNavigator />
         <Routes>
           <Route path="/" element={<LandingPage />} />
-          <Route path="/signin" element={<Signin />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/feature" element={<FeaturePage />} />
-          <Route path="/roadmap" element={<RoadMap />} />
-          <Route path="/pricing" element={<PricePage />} />
-          <Route path="/how-to-use" element={<HowToUsePage />} />
+          <Route path="signin" element={<Signin />} />
+          <Route path="signup" element={<Signup />} />
+          <Route path="feature" element={<FeaturePage />} />
+          <Route path="roadmap" element={<RoadMap />} />
+          <Route path="pricing" element={<PricePage />} />
+          <Route path="how-to-use" element={<HowToUsePage />} />
           <Route path="*" element={<LandingPage />} />
         </Routes>
       </Suspense>
